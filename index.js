7a08255276c7a118f05d56115d5fdcaf0caf5fbc
Tom Becker <tjbecker@me.com>
2019-06-18 17:07:43 +0700

    first edits

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+// paths
+
+app.get('/', (req, res) => {
+	res.send('hello')
+})
+
+//status
+app.get('/', (req, res) => {
+	res.status(200).send('hello')
+})
+
+//static files
+
+//serve every file inside client inside client folder as static
+app.use(express.static(path.join(__dirname, 'client')))
+
+app.get('/etsy', (req, res) => {
+	//send file from root/client/profile.txt
+	res.sendFile(path.join(__dirname,'./client/etsy.html'))
+})
+
+//run server
+app.listen(3000, () => {
+	console.log('server listening on port 3000')
+})
